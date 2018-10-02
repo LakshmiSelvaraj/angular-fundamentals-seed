58af13af0e1daf42750058d7f3a1d6f4cfe457db
lselvaraj <CaesarbruAug18!>
2018-10-02 12:38:39 +1000

    ngFor, ngClass and ngStyle

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 
+interface Child {
+  name: string,
+  age: number
+}
+
+interface Passenger {
+  name: string,
+  checkedIn: boolean,
+  checkedInDate: number | null,
+  children: Child[] | null
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
-  template: `
-    <div class="app">
-      Hello!
-    </div>
-  `
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  passengers: Passenger[];
+  constructor() {
+    this.passengers = [
+      {
+        name: 'Lakshmi Selvaraj',
+        checkedIn: true,
+        checkedInDate: 1538447072588,
+        children: [
+          {
+            name: 'Nila',
+            age: 1
+          }
+        ]
+      },
+      {
+        name: 'Myna',
+        checkedIn: false,
+        checkedInDate: null,
+        children:null
+      }
+    ]
+  }
 
 }
